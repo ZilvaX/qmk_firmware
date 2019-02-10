8ec9367ae5e798b9fb0a981de1c39fd2783777c3
Kevin <kevin.x.g@gmail.com>
2019-02-10 18:10:29 +1300

    Add backspace to lower layer and enter to upper

--- keyboards/preonic/keymaps/zilvax/keymap.c
+++ keyboards/preonic/keymaps/zilvax/keymap.c
@@ -17,6 +17,8 @@
 #include QMK_KEYBOARD_H
 #include "muse.h"
 #define ESC_CTL LCTL_T(KC_ESC)
+#define LL_BSPC LT(_LOWER, KC_BSPC)
+#define RL_ENT  LT(_LOWER, KC_ENT)
 
 enum preonic_layers {
   _QWERTY,
@@ -56,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,  \
   ESC_CTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,  \
-  BACKLIT, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
+  BACKLIT, KC_LCTL, KC_LALT, KC_LGUI, LL_BSPC, KC_SPC,  KC_SPC,  RL_ENT,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
 ),
 
 /* Colemak
