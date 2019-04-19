23320e91ca7c3f2a4ac0a3b90a40d48ccde78908
Kevin <kevin.x.g@gmail.com>
2019-04-19 16:36:26 +1200

    Return to default enter and backspace

--- keyboards/preonic/keymaps/zilvax/keymap.c
+++ keyboards/preonic/keymaps/zilvax/keymap.c
@@ -17,9 +17,6 @@
 #include QMK_KEYBOARD_H
 #include "muse.h"
 #define ESC_CTL LCTL_T(KC_ESC)
-#define LL_BSPC LT(_LOWER, KC_BSPC)
-#define RL_ENT  LT(_RAISE, KC_ENT)
-#define MO_ADJ  MO(_ADJUST)
 
 enum preonic_layers {
   _QWERTY,
@@ -59,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
   ESC_CTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,  \
-  KC_LCTL, KC_LCTL, KC_LALT, KC_LGUI, LL_BSPC, KC_SPC,  KC_SPC,  RL_ENT,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
+  KC_LCTL, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
 ),
 
 /* Colemak
@@ -122,7 +119,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, _______, KC_UP,   _______, _______, _______, _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, _______,  \
   _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, _______, \
   _______, _______, _______, _______, _______, _______, _______,S(KC_NUHS),S(KC_NUBS),_______, _______, _______, \
-  _______, _______, _______, _______, _______, _______, _______, MO_ADJ,  KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY  \
+  _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY  \
 ),
 
 /* Raise
@@ -143,7 +140,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, _______, _______, _______,  _______, _______, _______, KC_DEL,  KC_END,  KC_PGDN, _______, _______, \
   _______, _______, _______, _______,  _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, \
   _______, _______, _______, _______,  _______, _______, _______, KC_NUHS, KC_NUBS, _______, _______, _______, \
-  _______, _______, _______, _______,  MO_ADJ,  _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY  \
+  _______, _______, _______, _______,  _______, _______, _______,  _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY  \
 ),
 
 /* Adjust (Lower + Raise)
